9ad7ea9503f4e8e7715f3f1227e80cd9dc438e4f
ayouboss <ghzala.ayoub@gmail.com>
2020-03-12 16:44:34 -0400

    Update activity.go

--- activity/sha1/activity.go
+++ activity/sha1/activity.go
@@ -62,10 +62,11 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	
 	result := "ok"
 
-	err = context.SetOutput(ovResult, result)
-	if err != nil {
-		return false, err
-	}
+	//err = context.SetOutput(ovResult, result)
+	//if err != nil {
+	//	return false, err
+	//}
+	context.SetOutput(ovResult, result)
 
 	return true, nil
 }
